79c3a6343b9a114d037c9c1e1d2ec481f90b30e9
Samuel Longshak <difrentecho@gmail.com>
2019-06-19 06:51:02 +0100

    remove import of helper module.

--- flights/views.py
+++ flights/views.py
@@ -7,7 +7,6 @@ from rest_framework.response import Response
 from . import models, serializers, permissions
 from rest_framework_jwt.settings import api_settings
 from django.db.models import Q
-from .helpers.email import send_email
 
 jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
 jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
