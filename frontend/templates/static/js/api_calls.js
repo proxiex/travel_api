73b548daeb7614efd420c4548a6f2e010885b8d7
Samuel Longshak <difrentecho@gmail.com>
2019-06-25 23:52:49 +0100

    create search filight and result page

---
+++ frontend/templates/static/js/api_calls.js
@@ -0,0 +1,5 @@
+const baseUrl = 'http://127.0.0.1:8000/api/v1/search/?format=json'
+function searchFlight(from_location, to_location, depature_time) {
+
+    console.log('->>> ', from_location, to_location, depature_time)
+}
