507a21c9e55f5877b076404a3bc42eb7f15a938c
Samuel Longshak <difrentecho@gmail.com>
2019-06-27 08:08:51 +0100

    add user registration and login

--- users/tests/test_api.py
+++ users/tests/test_api.py
@@ -27,7 +27,7 @@ class UserAuthTest(APITestCase):
             content_type="application/json"
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertIn("message", response.data)
+        self.assertIn("error", response.data)
 
     def test_register_user(self):
         """Test user registration."""
