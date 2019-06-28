b285b2196f7997f6478278d990c4fe4cde8bcb01
Samuel Longshak <difrentecho@gmail.com>
2019-06-28 13:49:06 +0100

    updated baseUrl

--- frontend/templates/static/js/api_calls.js
+++ frontend/templates/static/js/api_calls.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://127.0.0.1:8000/api/v1/'
+const baseUrl = 'https://syne-travel.herokuapp.com/api/v1/'
 
 const post = (url, data) => fetch(url, {
     credentials: 'same-origin',
